ce689ff1f50a058f01868264f394ae6b12e1b1b9
Jeremiah Stafford <jstafford@slmdevice.com>
2019-10-29 21:14:27 -0400

    buildout of full stack app

---
+++ server/data/tableData.js
@@ -0,0 +1,11 @@
+const tableData = [
+  {
+    customerName: 'Jeremiah Stafford',
+    phoneNumber: '203-123-4567',
+    customerEmail: 'qwerty@123.com',
+    customerID: 'test1234',
+
+  },
+];
+
+module.exports = tableData;
---
+++ server/data/waitinglistData.js
@@ -0,0 +1,11 @@
+const waitData = [
+  {
+    customerName: 'John Smith',
+    phoneNumber: '453-124-2443',
+    customerEmail: 'ytrewq@321.com',
+    customerID: 'test12345',
+
+  },
+];
+
+module.exports = waitData;
